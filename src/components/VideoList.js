70ab8bac2a48d7790b3ebfcecb723bce7ebc3677
ada lovelace <williambookman@pursuit.org>
2023-12-27 18:27:00 -0500

    video list component completed

---
+++ src/components/VideoList.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { Grid } from "@mui/material";
+
+import VideoItem from "./Videoitem";
+
+const VideoList = ({ videos, onVideoSelect }) => { 
+    const listOfVideos = videos.map( (video, id) =>  <VideoItem onVideoSelect={onVideoSelect} key={id} video={video}/>)
+    
+    return (
+        <Grid container spacing={10}>
+        {listOfVideos}
+
+        </Grid>
+
+    )
+        
+}
+
+    
+
+export default VideoList;
