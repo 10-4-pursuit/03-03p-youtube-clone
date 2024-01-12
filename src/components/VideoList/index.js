aa36239c3457dbde5cda7981775623989517c45d
GyancarlosPinto <gyancarlospinto@pursuit.org>
2024-01-12 11:45:46 -0500

    Got VideoList Comp to show Video Thumbnails

--- src/components/VideoList/index.js
+++ src/components/VideoList/index.js
@@ -6,9 +6,10 @@ export default function VideoList({videos}) {
     return (
        <div>
            {videos.map(video => (
-               <div key={video.id.videoId}>
+               <div key={video.id.videoId} className="video-item">
                 <Link to={`/video/${video.id.videoId}`}>
-                {video.snippet.title}
+                    <img src={video.snippet.thumbnails.medium.url} alt={video.snippet.title} />
+                <div>{video.snippet.title}</div>
                 </Link>
                 </div>
            ))}
