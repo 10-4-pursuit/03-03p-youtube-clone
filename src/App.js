dac5d565d607c7445f3bee0832afe26a9d596c70
GyancarlosPinto <gyancarlospinto@pursuit.org>
2024-01-16 22:59:43 -0500

    Updated App.js with SearchRoute Comp

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
+// Import Dependencies and Components
 import './App.css';
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import VideoRoute from './components/VideoRoute';
+import SearchRoute from './components/SearchRoute';
 
 function App() {
 
+  // Return JSX for Rendering
   return (
     <div className="App">
       <header className="App-header">
+        {/* Use BrowserRouter (aliased as Router) to enable routing in the app. */}
         <Router>
+          {/* Navigation Setup with Link */}
           <nav>
             <Link to='/'>YouTube</Link>
           </nav>
 
+          {/* 
+          Define Routes Using Routes and Route:
+          - Routes is used to define all possible routes in the application.
+          - Route defines individual routes and their corresponding components.
+          */}
           <Routes>
-            <Route path='/' element={<Home />} exact />
+            {/* Define Home Route */}
+            <Route path='/' element={<Home />} />
+            {/* Define Video Route */}
             <Route path='/video/:videoId' element={<VideoRoute />} />
+            <Route path='/search' element={<SearchRoute />} />
           </Routes>
         </Router>
 
