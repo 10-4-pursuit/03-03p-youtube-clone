90fe15288f8b3439dd4685bd79035fc8e7621d62
GyancarlosPinto <gyancarlospinto@pursuit.org>
2024-01-16 19:37:19 -0500

    Updated dataFetching helper function with fetchTrendingVideos function

--- src/helpers/dataFetching.js
+++ src/helpers/dataFetching.js
@@ -1,7 +1,10 @@
+// Import Axios
 import axios from "axios";
 
 const dataFetching = async (searchTerm) => {
+    // Creating an HTTP GET Request to the YouTube Search API with Axios
     const response = await axios.get(`https://www.googleapis.com/youtube/v3/search`, {
+        // Specify Query Parameters
         params: {
             part: 'snippet',
             maxResults: 20,
@@ -10,9 +13,34 @@ const dataFetching = async (searchTerm) => {
         }
     })
 
+    // Return the Response Data
     return (
         response.data
     )
 };
 
-export default dataFetching;
+// Fetches the trending videos from YouTube.
+const fetchTrendingVideos = async () => {
+    try {
+        // Creating an HTTP GET Request
+        const response = await axios.get('https://www.googleapis.com/youtube/v3/videos', {
+            params: {
+                part: 'snippet,contentDetails,statistics',
+                chart: 'mostPopular',
+                regionCode: 'US',
+                maxResults: 10,
+                key: process.env.REACT_APP_API_KEY,
+            }
+        });
+        // Handling the Response
+        return (
+            response.data.items
+        )
+        
+        // Error Handling
+    } catch (error) {
+        console.error('Error fetching trending videos:', error);
+    }
+};
+
+export { dataFetching, fetchTrendingVideos };
