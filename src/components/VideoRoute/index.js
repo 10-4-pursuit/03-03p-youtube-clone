166095db34a59dc8b27d8b85af43ebe3af302ebb
GyancarlosPinto <gyancarlospinto@pursuit.org>
2024-01-16 22:27:29 -0500

    Built out VideoRoute Comp

--- src/components/VideoRoute/index.js
+++ src/components/VideoRoute/index.js
@@ -1,12 +1,16 @@
+// Import Dependencies and Components
 import React from "react";
 import { useParams } from "react-router-dom";
 import VideoPlayer from "../VideoPlayer";
 
 export default function VideoRoute() {
+    // Retrieve URL Parameter
     const { videoId } = useParams();
 
+    // Render the Component
     return (
         <div>
+            {/* Display the VideoPlayer Component */}
             <VideoPlayer videoId={videoId} />
         </div>
     );
