4107c6cec2c762f6cf5693c3183b58a68aad58ef
GyancarlosPinto <gyancarlospinto@pursuit.org>
2024-01-16 22:49:19 -0500

    Built SearchRoute Component

--- src/components/SearchRoute/index.js
+++ src/components/SearchRoute/index.js
@@ -1,33 +1,45 @@
 import React from "react";
 import SearchBar from "../SearchBar";
-import { useLocation } from "react-router-dom";
-import dataFetching from "../../helpers/dataFetching";
-
+import VideoList from "../VideoList";
+import { useSearchParams } from "react-router-dom";
+import { dataFetching } from "../../helpers/dataFetching";
 
 export default function SearchRoute() {
-    const [searchTerm, setSearchTerm] = React.useState("");
 
-    const location = useLocation();
+    // State for storing the list of videos fetched based on the search query
+    const [videos, setVideos] = React.useState([]);
 
-    // Function to parse query string
-    const useQuery = () => {
-        return new URLSearchParams(location.search)
-    }
+    const [searchParams] = useSearchParams();
+
+    const searchQuery = searchParams.get('searchQuery') || 'No Query';
 
     React.useEffect(() => {
-        const query = useQuery();
-        setSearchTerm(query.get("search"));
-    }, []); 
+        const getSearchResult = async () => {
+            const fetchData = await dataFetching(searchQuery);
+            setVideos(fetchData.items);
+        }
+        getSearchResult();
+    }, [searchQuery]);
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        dataFetching(searchTerm)
-    };
-    
-    return(
+    React.useEffect(() => {
+        const getSearchResult = async () => {
+            const fetchData = await dataFetching(searchQuery);
+            setVideos(fetchData.items);
+        }
+        getSearchResult(); 
+    }, [searchParams, searchQuery])
+
+    return (
         <div>
             {/* You can pass searchTerm to SearchBar if it needs the parameter */}
-            <SearchBar handleSubmit={handleSubmit} searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+            <SearchBar />
+
+            {videos.length > 0 && (
+                <>
+                    <h2>Search Results</h2>
+                    <VideoList videos={videos} />
+                </>
+            )}
         </div>
     )
 }
