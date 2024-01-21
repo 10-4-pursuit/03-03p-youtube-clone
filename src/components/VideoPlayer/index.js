d3605f299adf451a0070c7c9357d0180a328caae
GyancarlosPinto <gyancarlospinto@pursuit.org>
2024-01-21 17:13:00 -0500

    Updated VideoPlayer Component

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -1,13 +1,34 @@
 import React from "react";
 import LikeDislike from "../LikeDislike";
+import VideoContext from "../../helpers/VideoContext";
 
 export default function VideoPlayer({ videoId }) {
     const videoSrc = `https://www.youtube.com/embed/${videoId}`;
+
+    const {videos} = React.useContext(VideoContext)
+    
+    const video = videos.find((vid) => {
+        const id = vid.id.videoId || vid.id
+        return videoId === id
+    });
+
+    if (!video || !video.snippet) {
+        return <div>Video Not Found!</div>
+    }
 
     return (
         <div>
             <iframe src={videoSrc} allowFullScreen title="Video player" />
             <LikeDislike />
+            <label>
+                <p>{video.snippet.title}</p>
+            </label>
+            <label>
+                <p>{video.snippet.channelTitle}</p>
+            </label>
+            <label>
+                <p>{video.snippet.description}</p>
+            </label>
         </div> 
     );
 }
