68ea64375ca631749a7c9009c87d06cb68de8bea
GyancarlosPinto <gyancarlospinto@pursuit.org>
2024-01-21 16:34:47 -0500

    Implemented Updates caused by importing VideoContext Comp

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,20 +1,21 @@
 // Import Dependencies and Components
-import React from "react";
-import axios from "axios";
+import React, { useContext } from "react";
 import VideoList from "../VideoList";
 import SearchBar from "../SearchBar";
 import { fetchTrendingVideos } from "../../helpers/dataFetching";
+import VideoContext from "../../helpers/VideoContext";
 
 export default function Home() {
 
     // State for storing the list of currently trending videos
-    const [trendingVideos, setTrendingVideos] = React.useState([]);
+    // const [trendingVideos, setTrendingVideos] = React.useState([]);
+    const { getVideoData } = useContext(VideoContext)
 
     // Calls fetchTrendingVideos when the component is first mounted.
     React.useEffect(() => {
         const loadInitialVideos = async () => {
             const data = await fetchTrendingVideos();
-            setTrendingVideos(data);
+            getVideoData(data);
         }
         loadInitialVideos();
     }, []);
@@ -25,7 +26,7 @@ export default function Home() {
             <SearchBar />
 
             <h2>Trending Videos</h2>
-            <VideoList videos={trendingVideos} />
+            <VideoList />
 
             <h2>Music Videos</h2>
 
