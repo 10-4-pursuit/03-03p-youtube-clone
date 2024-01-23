5f67fdffa512d06dbd991dc7e7a2f488ff21ab0f
GyancarlosPinto <gyancarlospinto@pursuit.org>
2024-01-22 23:06:26 -0500

    Organized Format for VideoList

--- src/components/VideoList/index.js
+++ src/components/VideoList/index.js
@@ -1,26 +1,24 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import VideoPreview from "../VideoPreview";
-import VideoContext from "../../helpers/VideoContext";
 
-export default function VideoList() {
+export default function VideoList({ videos }) {
 
-    const {videos} = useContext(VideoContext)
-    
     return (
-       <div className="video-list">
-           {videos.map(video => {
-            const videoId = video.id.videoId || video.id
-            return (
-               <div key={videoId} className="video-item">
-                <Link to={`/video/${videoId}`}>
-                    <VideoPreview videoId={videoId} />
-                <div>{video.snippet.title}</div>
-                <div>{video.snippet.channelTitle}</div>
-                </Link>
-                </div>
-           )})}
-       </div>
+        <div className="video-list">
+            {videos.map(video => {
+                const videoId = video.id.videoId || video.id
+                return (
+                    <div key={videoId} className="video-item">
+                        <Link to={`/video/${videoId}`}>
+                            <VideoPreview videoId={videoId} />
+                            <div>{video.snippet.title}</div>
+                            <div>{video.snippet.channelTitle}</div>
+                        </Link>
+                    </div>
+                );
+            })}
+        </div>
     );
 }
 
