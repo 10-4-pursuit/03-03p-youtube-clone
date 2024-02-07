6a7b8160ce97c501990d22b41e350d7b45f0d10d
athomas227 <andrewthomas@pursuit.org>
2024-02-07 18:36:11 -0500

    added comments to videoplayer.js

--- src/Components/VideoPlayer/VideoPlayer.js
+++ src/Components/VideoPlayer/VideoPlayer.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getVideoById } from "../../Helpers/Data";
 
+// Component to display a video player
 export default function VideoPlayer({ vidId }) {
     const [videoInfo, setVideoInfo] = useState();
     const vidSrc = `https://www.youtube.com/embed/${vidId}`;
  
+    // Fetch video details when component mounts
     useEffect(() => {
         const getVideoDetails = async () => {
             const data = await getVideoById(vidId);
@@ -14,24 +16,29 @@ export default function VideoPlayer({ vidId }) {
         getVideoDetails();
     }, []);
 
+    // Render video player
     return !videoInfo || !videoInfo.snippet ? (
         <div className="video-player">
             <p className="no-video-found">No video found!</p>
         </div>
     ) : (
         <div className="video-player">
+            {/* Navigation links */}
             <div className='nav-bar'>
                 <Link to='/' className="home-link">Home</Link>
                 <Link to='/search' className="search-link">Search</Link>
             </div>
 
+            {/* Display video details */}
             <div className="video-info">
                 <h1 className="video-title">{videoInfo.snippet.title}</h1>
                 <h2 className="video-channel">{videoInfo.snippet.channelTitle}</h2>
             </div>
             
+            {/* Display video player */}
             <iframe src={vidSrc} allowFullScreen title="video player"/>
 
+            {/* Display video description */}
             <div className="video-description-container">
                 <p className="video-description">{videoInfo.snippet.description}</p>
             </div>
