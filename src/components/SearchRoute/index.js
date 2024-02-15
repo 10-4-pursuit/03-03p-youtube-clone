91d1b12d6da781bbd31b6b67667bf70f19ac77d1
GyancarlosPinto <gyancarlospinto@pursuit.org>
2024-02-14 21:15:29 -0500

    Implementing Pagination functionality in SearchRoute Comp

--- src/components/SearchRoute/index.js
+++ src/components/SearchRoute/index.js
@@ -2,28 +2,51 @@ import React from "react";
 import VideoList from "../VideoList";
 import { useSearchParams } from "react-router-dom";
 import VideoContext from "../../helpers/VideoContext";
+import Pagination from "../Pagination";
 
 export default function SearchRoute() {
 
-    const {searchResults, getSearchResults} = React.useContext(VideoContext)
+    const { searchResults, getSearchResults } = React.useContext(VideoContext)
 
     const [searchParams] = useSearchParams();
 
     const searchQuery = searchParams.get('searchQuery') || 'No Query';
 
+    const [currentPage, setCurrentPage] = React.useState(1);
+    const itemsPerPage = 10; // Set according to your preference
+
+    // Calculate current videos for pagination
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentVideos = searchResults.slice(indexOfFirstItem, indexOfLastItem)
+
+    // Change page
+    const paginate = pageNumber => setCurrentPage(pageNumber);
+
     React.useEffect(() => {
-        getSearchResults(searchQuery); 
+        if (searchQuery) {
+            getSearchResults(searchQuery);
+        }
     }, [searchQuery]);
 
     return (
         <div>
             {/* You can pass searchTerm to SearchBar if it needs the parameter */}
-
-            {searchResults.length > 0 && (
+            <h2>Search Results for "{searchQuery}</h2>
+            {searchResults.length > 0 ? (
                 <>
-                    <h2>Search Results</h2>
-                    <VideoList videos={searchResults} />
+                    <VideoList videos={currentVideos} displayMode="page" />
+                    <Pagination
+                        itemsPerPage={itemsPerPage}
+                        totalItems={searchResults.length}
+                        paginate={paginate}
+                        currentPage={currentPage}
+                    />
+
                 </>
+            ) : (
+                <p>No results found.</p>
+
             )}
         </div>
     )
