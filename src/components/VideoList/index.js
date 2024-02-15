a6c9fef3e6548f4bbe01e8f1243fcdb943b76313
GyancarlosPinto <gyancarlospinto@pursuit.org>
2024-02-14 21:18:44 -0500

    Updating VideoList Comp to work with Carousel and SearchRoute

--- src/components/VideoList/index.js
+++ src/components/VideoList/index.js
@@ -1,24 +1,38 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import VideoPreview from "../VideoPreview";
 import Carousel from "../Carousel";
 import './styles.css'
 
-export default function VideoList({ videos }) {
+export default function VideoList({ videos, displayMode = 'carousel' }) {
 
-    const carouselItems = videos.map(video => {
-        return {
-            title: video.snippet.title,
-            description: video.snippet.description,
-            videoId: video.id.videoId || video.id,
-            channelTitle: video.snippet.channelTitle
-        };
-    });
-
+    const renderVideosForPage = () => {
+        return videos.map(video => {
+            
+            const videoId = video.id.videoId || video.id;
+            return (
+                <div key={videoId} className="video-item">
+                    <Link to={`/video/${videoId}`} className="video-link">
+                        <VideoPreview videoId={videoId} />
+                    </Link>
+                    <div className="video-details">
+                        <div className="video-title">{video.snippet.title}</div>
+                        <div className="video-channel-title">{video.snippet.channelTitle}</div>
+                    </div>
+                </div>
+            );
+        });
+    };
+    console.log(videos)
     return (
-        <div className="video-list">
-            <Carousel videos={carouselItems} />
+        <div className={displayMode === 'carousel' ? 'video-carousel' : 'video-list-page'}>
+            {displayMode === 'carousel' ? (
+                <Carousel videos={videos} />
+            ) : (
+                renderVideosForPage()
+            )}
         </div>
     );
-}
+};
 
 
